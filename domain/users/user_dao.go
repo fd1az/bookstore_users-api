266846c12e8f17b2b62bc97cc6b6ff1ebeff2ca2
Fdiaz7 <facu.diaz777@gmail.com>
2019-12-14 13:18:25 -0300

    admin dates

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -3,6 +3,7 @@ package users
 import (
 	"fmt"
 
+	"github.com/fdiaz7/bookstore_users-api/utils/date_utils"
 	"github.com/fdiaz7/bookstore_users-api/utils/errors"
 )
 
@@ -31,6 +32,8 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exist", user.Id))
 	}
+	user.DateCreated = date_utils.GetNowString()
+
 	usersDB[user.Id] = user
 	return nil
 }
